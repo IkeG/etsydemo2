2f855c6da17092df55bf60a1143905e68ed478e3
Ike <F.Geiken@gmail.com>
2014-05-23 19:44:42 -0700

    Finish setup for dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,18 @@
 class Listing < ActiveRecord::Base
-
-	 
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
+		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+	else
 	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
 		:storage => :dropbox,
-	    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	 validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
+	    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	 	:path => ":style/:id_:filename"
+	 	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+	end
 end
+
+ 
+
 
 
 require 'paperclip/media_type_spoof_detector'         
